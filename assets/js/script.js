100a9d621d67dbde62fca87c580625dd6ef6ef2e
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-19 21:41:26 -0500

    edits to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,36 @@
 var startBtn = document.querySelector('.start-button');
 var wordBlanksEl = document.querySelector('.word-blanks');
 
+
+var validChar = 'abcdefghijklmnopqrstuvwxyz'
 var words = ['birthday', 'javascript', 'object', 'function', 'variable', 'localstorage', 'eventlister', 'timeout'];
-var word 
+var word ;
+var guessChar = [];
 //socre variable
 //timeleft variable
+
+function handleKeyUp(event){
+    console.log(event);
+    //validate the key
+    if (validChar.includes(event.key)){
+        //keep track of the character that was guessed
+        guessChar.push(event.key)
+        //rerender the word
+        renderChars()
+    }
+}
 
 function renderChars(){
     var str = ''
     for(var i = 0; i < word.length; i++){
-        //if right 
+        var letter = word[i];
+        if(guessChar.includes(letter) ){
+            str += letter
             //push charcter into our str
-        //else 
+        }else{
             //addh an _ into str
+        }
+            
        str += '_ '
        
 
@@ -32,3 +50,4 @@ function startRound(){
 
 startBtn.addEventListener('click', startRound)
     
+document.addEventListener('keyup', handleKeyUp)
