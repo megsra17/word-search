876deaed45c50d8c5e112045924c727a48d0bacb
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-19 21:59:11 -0500

    edits to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 //select element from the dom 
 var startBtn = document.querySelector('.start-button');
 var wordBlanksEl = document.querySelector('.word-blanks');
+var scoreEl =document.querySelector('.score')
 
 
 var validChar = 'abcdefghijklmnopqrstuvwxyz'
 var words = ['birthday', 'javascript', 'object', 'function', 'variable', 'localstorage', 'eventlister', 'timeout'];
 var word ;
 var guessChar = [];
-//socre variable
+var score = 0;
+
 //timeleft variable
+
+
+function checkWord(){
+    //compare word and wordblanks text content
+    var wordDom =wordBlanksEl.textContent.split(' ').join('');
+    if (word === wordDom){
+        score ++
+        scoreEl.textContent = score;
+    }
+}
 
 function handleKeyDown(event){
     console.log(event);
@@ -27,21 +39,16 @@ function renderChars(){
         var letter = word[i];
         if(guessChar.includes(letter) ){
             str += letter
-            //push charcter into our str
         }else{
-            //addh an _ into str
             str += '_ '
-        }
-            
-       
-       
-
+        }       
     }
-    //set textContent of wordBlanksEl to be str
     wordBlanksEl.textContent = str.trim();
+    checkWord();
 }
 
 function startRound(){
+    guessChar = [];
     var randomIndex = Math.floor(Math.random() * words.length);
     word = words[randomIndex];
     renderChars()
