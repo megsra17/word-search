bf4a7319e62f85cc6787b55ad9c9b3317cf3187e
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-19 21:11:55 -0500

    edits to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,20 @@ var startBtn = document.querySelector('.start-button');
 var wordBlanksEl = document.querySelector('.word-blanks');
 
 var words = ['birthday', 'javascript', 'object', 'function', 'variable', 'localstorage', 'eventlister', 'timeout'];
+var word 
 //socre variable
 //timeleft variable
 
-
-
-function startRound(){
-    console.log('Start')
-
-    
-    //get random words from word array  
-}
-
 function renderChars(){
 //word-blanks would reset _ for each letter of the new word
+}
+
+function startRound(){
+    var randomIndex = Math.floor(Math.random() * words.length);
+    word = words[randomIndex];
+    renderChars()
+    
+    //get random words from word array  
 }
 
 startBtn.addEventListener('click', startRound)
